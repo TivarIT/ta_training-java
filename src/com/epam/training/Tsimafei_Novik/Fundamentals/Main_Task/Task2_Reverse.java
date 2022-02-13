eab1ce2871c03ee3cddb4fa70eb6b3285d58961c
Tsimafei Novik <timoxa.novik03@gmail.com>
2022-02-13 21:24:57 +0300

    fix

--- src/com/epam/training/Tsimafei_Novik/Fundamentals/Main_Task/Task2_Reverse.java
+++ src/com/epam/training/Tsimafei_Novik/Fundamentals/Main_Task/Task2_Reverse.java
@@ -1,10 +1,20 @@
 package com.epam.training.Tsimafei_Novik.Fundamentals.Main_Task;
+import java.util.Scanner;
 
 public class Task2_Reverse {
     //2. Отобразить в окне консоли аргументы командной строки в обратном порядке.
     public static void main(String[] args) {
-        //Аргументами выступают Arg1 Arg2 Arg3
+        /*Если под "аргументами" понимается значения в параметре args, то Arg1 Arg2 Arg3 это наши аргументы ф-ции main
         for(int i = args.length - 1; i >= 0; i--)
             System.out.print(args[i] + " "); //Arg3 Arg2 Arg1
+         */
+        //Но если имелось ввиду просто ввод данных с клавиатуры, то так
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите данные: ");
+        String input = in.nextLine();
+        StringBuffer buffer = new StringBuffer(input);
+        buffer.reverse();
+        System.out.print("В обратном порядке: ");
+        System.out.print(buffer);
     }
 }
